9ff845e95a71ffc6fad5dfad3287487684f4d093
Matthew Michilot <matthew.michilot@gmail.com>
2022-01-04 13:45:07 -0800

    Inital commit

---
+++ Lab_1/EE419_HF_exp1_proto.m
@@ -0,0 +1,105 @@
+% EE419_HF_exp1_proto
+% COMPUTE AND PLOT THE FREQUENCY RESPONSE OF AN IIR FILTER
+% H(z) = z / (z-0.9)
+
+% Make a digital frequency variable with 1001 points 
+% for Principle Range F =-1/2 to +1/2, with spacing .001 cycles/sample
+
+%Freq=[-0.5 -0.499 -0.498 ......... 0.499  0.5];  % 1001 values?! YIKES!!
+
+%Freq=linspace(first_val, last_val, Num_of_points); 
+% For LINSPACE: Increment =(last_val - first_val) / (Num_of_points-1) 
+%Freq=linspace(-.5, .5, 1001); % 1001 points, from -1/2 to +1/2, Incr=.001
+
+%Freq=[first_val : increment : last_val]; 
+% NOTE: # of Points =[(last_val-first_val) / increment]+1 
+                    % 1001 points, from -1/2 to +1/2, increment=.001
+Freq = -0.5:.001:0.5;
+
+% Compute the values of the COMPLEX DTFT frequency response at these frequencies
+
+HF = exp(1i*2*pi*Freq)./(exp(1i*2*pi*Freq)-0.9);
+
+%% LINEAR FREQUENCY RESPONSE PLOT
+% Plot the magnitude and phase responses on a linear freq scaleOp
+
+% Open a new Figure #1:
+figure(1)
+% Plot the Magnitude Response 
+subplot(2,1,1)  % Display plots in 2 rows / 1 column; This is the 1st plot.
+
+% Plot the magnitude of HF on a linear scale
+plot(Freq, abs(HF), 'k--');
+grid on
+xlabel('Digital Frequency  F (cycles/sample)')
+ylabel('Magnitude Response')
+title('Frequency Response of Filter')
+
+% Plot the Phase Response below the Magnitude Response
+subplot(2,1,2) % Display plots in 2 rows / 1 column; This is the 2nd plot.
+
+% Plot the Phase Angle vs Frequency     
+                    % Normalize angle radian values by pi radians
+plot(Freq, (angle(HF)./pi)*180, 'g', 'LineWidth', 2)
+grid on
+xlabel('Digital Frequency  F (cycles/sample)')
+ylabel('Phase Response (degrees)')
+
+
+%% PLOT dB (LOG AMPLITUDE) SCALE FREQUENCY RESPONSE PLOT in a new figure
+% copy, paste, modify plot commands from above
+
+% Open a new Figure #2:
+figure(2)
+% Plot the Magnitude Response 
+subplot(2,1,1)  % Display plots in 2 rows / 1 column; This is the 1st plot.
+
+% Plot the magnitude of HF on a dB scale
+plot(Freq, 20*log10(abs(HF)));
+grid on
+xlabel('Digital Frequency  F (cycles/sample)')
+ylabel('Magnitude Response')
+title('Frequency Response of Filter')
+
+% Plot the Phase Response below the Magnitude Response
+subplot(2,1,2) % Display plots in 2 rows / 1 column; This is the 2nd plot.
+
+% Plot the Phase Angle vs Frequency     
+                    % Normalize angle radian values by pi radians
+plot(Freq, (angle(HF)./pi)*180, 'g', 'LineWidth', 2)
+grid on
+xlabel('Digital Frequency  F (cycles/sample)')
+ylabel('Phase Response (degrees)')
+
+%% Use Matlab freqz() function to plot frequency response
+% [H,W] = freqz(B,A,N) 
+%   B = H(z) numberator coefficients
+%   A = H(z) denominator coefficients
+%   N = # of complex H(F) data points to compute 
+%   H = complex H(F) values
+%   W = radian digital frequency = 2*pi*F
+
+% Get 500 points for the plot
+B = [1, 0];
+A = [1, -0.9];
+[H, W] = freqz(B,A,500);
+
+% Plot the magnitude and phase response (copy, paste, modify)
+figure(3)
+plot(W/(2*pi),20*log10(abs(H)));
+xlabel('Digital Freq (cycles/sample)')
+
+% Find the peak response and peak response frequency
+[peak_response, peak_response_index] = max(abs(HF))
+% No ';' at end so answers print to screen
+peak_response_freq = Freq(peak_response_index)
+
+% Find the minimum magnitude response and min response frequency
+[min_response, min_response_index] = min(abs(HF))
+% No ';' so answers print to screen
+min_response_freq = Freq(min_response_index)
+
+% Interrogate Figure 1 to check the results
+
+% COPY FIGURES TO A WORD DOCUMENT WHEN DONE
+
---
+++ Lab_1/plot_freq_response.m
@@ -0,0 +1,14 @@
+function plot_freq_response(Fd, HF, fsample, figure_num)
+%plot_freq_response Plots the frequency response of H(F)
+%   Fd = an array of digital frequency values (in units of 
+%        cycles/sample) that correspond to the H(F) frequency 
+%        response data values 
+%   HF = an array of complex H(F) DTFT frequency response values to 
+%        plot 
+%   fsample = sampling frequency (in units of samples / second) 
+%   figure_num = number of the 1st figure to use for the two plots 
+
+
+
+end
+
