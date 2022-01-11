f0d0695b80f62ed6397207f607c2c9b03465151f
Matthew Michilot <matthew.michilot@gmail.com>
2022-01-11 13:11:59 -0800

    Created unit_sample_response

---
+++ Lab_2/unit_sample_response.m
@@ -0,0 +1,36 @@
+function [hn, n] = unit_sample_response(Bk, Ak, number_of_samples, figure_number)
+% function [hn, n]=unit_sample_response(Bk, Ak, number_of_samples, figure_number)
+% Plots and returns a unit sample response sequence
+%
+%   Inputs:
+%   Bk = a list of the B k coefficients of the filter difference equation
+%        (coefficients of the “x” terms)
+%   Ak = a list of the A k coefficients of the filter difference equation
+%        (coefficients of the “y” terms)
+%   number_of_samples = # of unit sample response sequence samples to find
+%   figure_number = # figure for the hn sequence plot
+%
+%   Outputs:
+%   hn = has the unit sample response sequence values
+%   n = has the corresponding sample indices (starting at 0);
+%
+%   Depends:
+%   unit_sample.m
+
+% Generate unit sample sequence x[n]
+[xn, n] = unit_sample(number_of_samples);
+
+% Generate unit sample response sequence h[n] via filter()
+hn = filter(Bk, Ak, xn);
+
+% Plot h[n]
+figure(figure_number)
+stem(n, hn, '.')
+grid on
+xlabel('n')
+ylabel('h[n]')
+title('Unit Sample Response h[n]')
+
+
+
+end
